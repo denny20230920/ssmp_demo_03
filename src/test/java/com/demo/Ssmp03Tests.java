4908173527ce097fbc8b268b5a09cc5bf0cd604c
denny20230920 <145538884+denny20230920@users.noreply.github.com>
2024-05-27 18:09:43 +0800

    Update Ssmp03Tests.java

--- src/test/java/com/demo/Ssmp03Tests.java
+++ src/test/java/com/demo/Ssmp03Tests.java
@@ -3,10 +3,10 @@ package com.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class Ssmp03Tests {
 
-    @Test
+   // @Test
     void contextLoads() {
     }
 
